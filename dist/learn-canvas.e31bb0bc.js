22e3d575c82673d9918a2f745534ad0003265987
raye <38679090+nascenticity@users.noreply.github.com>
2019-04-11 13:44:09 -0500

    crop to first sprite

--- dist/learn-canvas.e31bb0bc.js
+++ dist/learn-canvas.e31bb0bc.js
@@ -123,10 +123,19 @@ window.onload = function () {
   var context = canvas.getContext('2d');
   canvas.width = 300;
   canvas.height = 400;
-  var sprite = new Image();
+  var sprite = new Image(); // cx & cy = context x & y coordinates
+
+  var cx = 0;
+  var cy = 0; // sx & sy = sprite x & y coordinates
+
+  var sx = 0;
+  var sy = 0; // sw & sh = sprite width = height;
+
+  var sw = 32;
+  var sh = 32;
 
   sprite.onload = function () {
-    context.drawImage(sprite, 0, 0);
+    context.drawImage(sprite, sx, sy, sw, sh, cx, cy, 32, 32);
   };
 
   sprite.src = './bird_2_black.png';
--- index.js
+++ index.js
@@ -4,9 +4,18 @@ window.onload = function(){
     canvas.width = 300;
     canvas.height = 400;
     var sprite = new Image();
+    // cx & cy = context x & y coordinates
+    var cx = 0;
+    var cy = 0;
+    // sx & sy = sprite x & y coordinates
+    var sx = 0;
+    var sy = 0;
+    // sw & sh = sprite width = height;
+    var sw = 32;
+    var sh = 32;
 
     sprite.onload = function() {
-        context.drawImage(sprite, 0,0);
+        context.drawImage(sprite, sx, sy, sw, sh, cx, cy, 32,32);
     }
 
     sprite.src = './bird_2_black.png';
