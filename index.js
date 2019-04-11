b5aa3d85ccc0dec157a0e8059f12af431a75a218
raye <38679090+nascenticity@users.noreply.github.com>
2019-04-11 11:09:22 -0500

    change of plans - not learning webgl till later

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function main () {
-    const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('#canvas');
 
-const context = canvas.getContext('webgl');
-console.log(context);
-// getContext is a built in function with the canvas
-
-    if (context === null){
-        alert("Could not initialize WebGL. Your browser or device may not support it.")
-    }
-}
-
-main();
+const context = canvas.getContext('2d');
