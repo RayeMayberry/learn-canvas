48e72efebed574d8a3ff6e13f0be2c857b899d39
Raye <rayemayberry@yahoo.com>
2019-04-12 23:02:59 -0500

    this is good for now

--- index.js
+++ index.js
@@ -8,35 +8,45 @@ window.onload = function(){
     blackBirdImg.src = './bird_2_black.png';
 
     class Sprite{
-        constructor(src, width, height, frames){
+        constructor(src, width, height, frames, cycles){
             this.src = src;
             this. width = width;
             this.height = height;
             this.frames = frames;
+            this.cycles = cycles,
             this.index = [0,0]
         }
     };
-
-    var bird = new Sprite(blackBirdImg, 32, 32, 3);
-
-    // cx & cy = context x & y coordinates
-    var cx = canvas.width - (bird.width);
-    var cy = 0;
     
-    function animate(sprite, position, framerate) {
+    function animate(sprite, position, cycle, framerate) {
         let i = 0;
         setInterval(()=>{
             
             sprite.index[0] = i * sprite.width;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(sprite.src, sprite.index[0], sprite.index[1], sprite.width,sprite.height, position[0], position[1], sprite.width, sprite.height);
-            i++;
-            if(i === 3){
-                i = 0;
-            }
-        }, 1000/framerate);
+
+            sprite.index[1] = cycle
+            
+            ctx.clearRect(
+                position[0], position[1], sprite.width, sprite.height
+                );
+                ctx.drawImage(
+                    sprite.src, 
+                    sprite.index[0], 
+                    sprite.index[1]*sprite.height,
+                    sprite.width,sprite.height, 
+                    position[0], 
+                    position[1], 
+                    sprite.width, 
+                    sprite.height);
+                    i++;
+                    if(i === 3){
+                        i = 0;
+                    }
+                }, 1000/framerate);
     }
-    
-    blackBirdImg.onload =  animate(bird, [cx, cy], 5);
-    
+            
+    var bird = new Sprite(blackBirdImg, 32, 32, 3, 8);
+            
+    animate(bird, [0,0], 3, 5);
+            
 }
