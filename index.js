5a34529f18466a233b29495b2868fc5b7b78fa28
Raye <rayemayberry@yahoo.com>
2019-04-12 22:24:25 -0500

    refactor into sprite class & animate function

--- index.js
+++ index.js
@@ -2,34 +2,41 @@
 window.onload = function(){
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = 400;
-    canvas.height = 300;
+    canvas.width = 32 * 12;
+    canvas.height = 32 * 8;
     const blackBirdImg = new Image();
     blackBirdImg.src = './bird_2_black.png';
-    // sprite width & height can also be used to iterate thru the spritesheet
-    const width = 32;
-    const height = 32;
-    const scale = 1;
+
+    class Sprite{
+        constructor(src, width, height, frames){
+            this.src = src;
+            this. width = width;
+            this.height = height;
+            this.frames = frames;
+            this.index = [0,0]
+        }
+    };
+
+    var bird = new Sprite(blackBirdImg, 32, 32, 3);
+
     // cx & cy = context x & y coordinates
-    var cx = canvas.width - (width*scale);
+    var cx = canvas.width - (bird.width);
     var cy = 0;
-    // sx & sy = sprite x & y coordinates
-    var sx = 0;
-    var sy = 0;
     
-    
-    blackBirdImg.onload = function() {
+    function animate(sprite, position, framerate) {
         let i = 0;
         setInterval(()=>{
             
-            sx = i * width;
+            sprite.index[0] = i * sprite.width;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(blackBirdImg, sx, sy, width, height, cx, cy, width*scale, height*scale);
+            ctx.drawImage(sprite.src, sprite.index[0], sprite.index[1], sprite.width,sprite.height, position[0], position[1], sprite.width, sprite.height);
             i++;
             if(i === 3){
                 i = 0;
             }
-        }, 200);
-        
+        }, 1000/framerate);
     }
+    
+    blackBirdImg.onload =  animate(bird, [cx, cy], 5);
+    
 }
