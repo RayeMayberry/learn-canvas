a455e7cf808d4c87a6cfa799700d71a6adaea9df
Raye <rayemayberry@yahoo.com>
2019-04-12 22:06:44 -0500

    reset working code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 window.onload = function(){
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
@@ -18,10 +19,17 @@ window.onload = function(){
     
     
     blackBirdImg.onload = function() {
-        for(i = 0; i < 3; i++){
+        let i = 0;
+        setInterval(()=>{
+            
+            sx = i * width;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(blackBirdImg, i * width, sy, width, height, cx, cy, width*scale,height*scale);
-        }
+            ctx.drawImage(blackBirdImg, sx, sy, width, height, cx, cy, width*scale, height*scale);
+            i++;
+            if(i === 3){
+                i = 0;
+            }
+        }, 200);
         
     }
 }
